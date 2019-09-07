02c4d061a4c20a3eb3e5307fb555138f73166a59
237617561@qq.com <*n19900814x*>
2019-09-07 09:06:44 +0800

    debug

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import cluster from 'cluster';
 import { cpus } from 'os';
+import { IS_CLUSER } from './server/constant';
 
-if (cluster.isMaster) {
+if (IS_CLUSER && cluster.isMaster) {
   let workerLength = cpus().length;
   while (workerLength > 0) {
     cluster.fork();
@@ -9,6 +10,9 @@ if (cluster.isMaster) {
   }
   require('./server/master')
 } else {
+  if (!IS_CLUSER) {
+    require('./server/dns');
+  }
   require('./server/cn');
   require('./server/en');
 }
