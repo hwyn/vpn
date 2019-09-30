e50385e2d91fc5941b99e3ac70fec71fb612446e
HWUYing <237617561@qq.com>
2019-10-01 02:09:41 +0800

    remove

--- src/server/util/index.ts
+++ src/server/util/index.ts
@@ -3,6 +3,5 @@ export * from './event-emitter';
 export * from './buffer-util';
 export * from './package-separation';
 export * from './package-manage';
-export * from './abnormal-manage';
 export * from './client-hello';
 export * from './event-communication';
